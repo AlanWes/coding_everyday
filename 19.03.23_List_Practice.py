5b0c749b59382930065bf46d4a2febc296d2955d
alanwes <125877927+AlanWes@users.noreply.github.com>
2023-03-19 21:57:52 +0100

    list_practice

---
+++ 19.03.23_List_Practice.py
@@ -0,0 +1,12 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# vegetable = input("Your the most liked vegetable? ")
+
+# states_of_america.append(vegetable)
+
+# print(states_of_america)
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+math = [(x, y) for x in[5,6,7] for y in(8,9,1) if x != y]
+print(math)
